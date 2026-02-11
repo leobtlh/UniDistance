1d660d12682cd8b2a7d38d21bd0bab31aa3fc276
Leo Botelho <leo.botelho@altux.ch>
2026-02-11 09:44:08 +0100

    Homework 2: (ii)

--- M01_/exercises/homework02_ii.py
+++ M01_/exercises/homework02_ii.py
@@ -1,5 +1,15 @@
 import numpy as np
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 
-st = "ii"
-print(f"Homework #2 :\n     {st}.")
+# start: 0, end: 20, step: 0.2
+x = np.arange(0., 20., 0.2)
+
+# fct. f(x) = -x + 10
+f = -x + 10
+
+plt.plot(x, f)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('Homework 2: (ii)')
+plt.grid(True)
+plt.show()
